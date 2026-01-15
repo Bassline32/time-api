df94f50ad9b2f5bd1a785e7423a77e20d31b54a1
Bassline32 <ivandzen911@Gmail.com>
2026-01-15 21:21:23 +0300

    Готовое задание

--- src/main/java/controller/NoteController.java
+++ src/main/java/controller/NoteController.java
@@ -1,18 +1,19 @@
 package controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.Note;
 import service.NoteService;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/api/v1/notes")
 public class NoteController {
-
 
     private final NoteService noteService;
 
@@ -21,10 +22,11 @@ public class NoteController {
     }
 
     // Cоздаём заметку и передаём серверу данные от юзера из класса Note
+    //http status
     @PostMapping
-    public Note createNote(@RequestBody Note note) {
+    public ResponseEntity<?> createNote(@RequestBody Note note) {
         noteService.create(note);
-        return note;
+        return ResponseEntity.status(HttpStatus.CREATED).body(note);
     }
 
     @GetMapping
@@ -32,9 +34,12 @@ public class NoteController {
         return noteService.findAll();
     }
 
+    //http status
     @GetMapping("/{id}")
-    public Note singleNote(@PathVariable Long id) {
-        return noteService.notes.getOrDefault(id, null);
+    public ResponseEntity<?> singleNote(@PathVariable Long id) {
+        Note note = noteService.notes.get(id);
+        if (note == null) return ResponseEntity.notFound().build();
+        else return ResponseEntity.ok(note);
     }
 
     @PutMapping("/{id}")
@@ -42,9 +47,11 @@ public class NoteController {
         return noteService.update(id, updateNote);
     }
 
+    //http status
     @DeleteMapping("/{id}")
-    public void deleteNOte(@PathVariable Long id) {
-        noteService.delete(id);
+    public ResponseEntity<Void> deleteNOte(@PathVariable Long id) {
+        if (noteService.delete(id)) return ResponseEntity.noContent().build();
+        else return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/search")
@@ -52,17 +59,33 @@ public class NoteController {
         return noteService.search(keyword);
     }
 
-
     @PostMapping("/{id}/remind")
     public String remind(@PathVariable Long id, @RequestParam(defaultValue = "30") Integer minutes) {
         Note note = noteService.notes.get(id);
         return " Напоминание о заметке " + note.getTitle() + " через " + minutes + " минут ";
+    }
 
-        //Object тип возвращаемого значения метода в JSON или в text
+    @GetMapping("/export/json")
+    public List<Note> exportJson() {
+        return noteService.findAll();
+    }
 
+    @GetMapping("/export/text")
+    public String exportText () {
+        return noteService.findAll()
+                .stream()
+                .map(note -> note.getTitle() + " " + note.getContent())
+                .collect(Collectors.joining("/n"));
+    }
+
+    @PostMapping("/batch")
+    public List<Note> batchIn(@RequestBody List<Note> notesToAdd) {
+        for (Note n : notesToAdd) {
+            noteService.create(n);
+        }
+        return notesToAdd;
     }
 }
-
 
 
 
